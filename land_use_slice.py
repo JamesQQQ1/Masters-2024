1955b12eab5868b76d7154dd02599cfcb315829e
James Quessy <151444898+JamesQQQ1@users.noreply.github.com>
2024-05-01 10:40:19 +0100

    Update land_use_slice.py

--- land_use_slice.py
+++ land_use_slice.py
@@ -3,22 +3,22 @@ import time
 
 start = time.time()
 
-# Define the geographic boundaries of the UK
+# Boundaries of the UK
 min_lon, max_lon = -10, 2
 
-# Path to the original NetCDF file
+# Path to files
 file_path = '/Users/jamesquessy/Desktop/Uni Work/Masters/Reasearch Project/Code/Power_Generation/land_use/land_use.nc'
 output_path = '/Users/jamesquessy/Desktop/Uni Work/Masters/Reasearch Project/Code/Power_Generation/land_use/sliced_land_use_uk.nc'
 
-# Open the NetCDF file
+# Open the file
 with xr.open_dataset(file_path) as ds:
-    # Check the actual range of latitude and longitude in the dataset
+    # Check the range of latitude and longitude in the file
     print("Actual latitude range:", ds.lat.min().values, "to", ds.lat.max().values)
 
-    # Select the subset of the data for the UK
+    # Select the UK
     sliced_ds1 = ds.sel(lon=slice(min_lon, max_lon))
 
-    # Save the sliced data to a new file
+    # Save the UK to a new file
     sliced_ds1.to_netcdf(output_path)
 
 end = time.time()
